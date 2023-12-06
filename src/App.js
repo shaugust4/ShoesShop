aa3ec65307eb01fe0bad49463d6e9ee1616775e0
shaugust4 <osh8779@gmail.com>
2023-12-06 17:53:21 +0900

    shoeshop1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import "./App.css";
+import bg from "./img/bg.jpg";
+import { useState } from "react";
+import data from "./data.js";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./pages/shopDetail.js";
 
 function App() {
+  let [shoes] = useState(data);
+  let navigate = useNavigate();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">shoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Detail
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div
+                className="main-bg"
+                style={{ backgroundImage: "url(" + bg + ")" }}
+              ></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map((a, i) => {
+                    return <Card shoes={shoes[i]} i={i} />;
+                  })}
+                </div>
+              </div>
+            </>
+          }
+        ></Route>
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버 정보</div>} />
+          <Route path="location" element={<div>위치 정보</div>} />
+        </Route>
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
 
+function Event() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+function About() {
+  return (
+    <div>
+      <h4>Company</h4>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={
+          "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
+        }
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+    </div>
+  );
+}
 export default App;
---
+++ src/data.js
@@ -0,0 +1,31 @@
+let data = [
+  {
+    id: 0,
+    title: "White and Black",
+    content: "Born in France",
+    price: 120000,
+  },
+
+  {
+    id: 1,
+    title: "Red Knit",
+    content: "Born in Seoul",
+    price: 110000,
+  },
+
+  {
+    id: 2,
+    title: "Grey Yordan",
+    content: "Born in the States",
+    price: 130000,
+  },
+
+  {
+    id: 3,
+    title: "Blue Rain",
+    content: "Born in New Zealand",
+    price: 180000,
+  }
+];
+
+export default data;
